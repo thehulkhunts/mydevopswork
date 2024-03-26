5ef3f76fba6bcd4ef7fd7ffeceb5f681114aa678
Vinay Jadhav <43322698+thehulkhunts@users.noreply.github.com>
2024-03-26 19:11:23 +0530

    Create eksctl-kubectl.sh

---
+++ EKSCTL-KUBECTL/eksctl-kubectl.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin 
+kubectl version --short --client
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
